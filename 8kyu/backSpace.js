49e1d135e3ec152dee59c92276062c3183685098
Rex Hector <reginald.hector@outlook.com>
2023-04-21 19:51:09 -0400

    Submitting Code Wars 4/21/2023

---
+++ 8kyu/backSpace.js
@@ -0,0 +1,16 @@
+function processStringWithBackspace(s) {
+      const stack = [];
+      
+      for (const char of s) {
+          if (char === "#") {
+              if (stack.length > 0) {
+                  stack.pop();
+              }
+          } else {
+              stack.push(char);
+          }
+      }
+      
+      return stack.join("");
+  }
+  
